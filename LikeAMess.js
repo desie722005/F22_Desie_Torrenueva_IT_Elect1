d112a89f1911eb6926556e4140ddd209d29dcec2
desie722005 <torrenuevadesie@gmail.com>
2025-09-03 20:59:45 +0800

    Initial commit

---
+++ LikeAMess.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const LikeAMess = () => {
+  return (
+    <View style={styles.container}>
+      <Text>LikeAMess</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default LikeAMess;
