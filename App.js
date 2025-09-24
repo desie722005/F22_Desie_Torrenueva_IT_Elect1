3b6d259d7b72441d1b3d4f03dbb937104d09cffb
desie722005 <torrenuevadesie@gmail.com>
2025-09-24 15:27:24 +0800

    edit again

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Cherry Ruth A. Taghoy</Text>
+      <Text>Desie B. Torrenueva</Text>
       <StatusBar style="auto" />
     </View>
   );
